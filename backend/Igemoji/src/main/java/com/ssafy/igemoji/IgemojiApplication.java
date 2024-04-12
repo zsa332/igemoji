f23fb920f615456e6d1df2fa5a26734623e54561
서성원 <zsa332@gmail.com>
2024-04-12 13:59:45 +0900

    Initial Backend

---
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/IgemojiApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.igemoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgemojiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgemojiApplication.class, args);
+    }
+
+}
---
+++ backend/Igemoji/src/test/java/com/ssafy/igemoji/IgemojiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.igemoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgemojiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
