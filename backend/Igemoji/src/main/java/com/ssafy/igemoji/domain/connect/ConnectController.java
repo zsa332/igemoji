b0d82148412f61349c34b689dea6853f3eb06663
서성원 <zsa332@gmail.com>
2024-04-24 10:52:29 +0900

    feat : Add ConnectController for Connect Test

---
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/connect/ConnectController.java
@@ -0,0 +1,17 @@
+package com.ssafy.igemoji.domain.connect;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/connect")
+public class ConnectController {
+    @GetMapping()
+    public ResponseEntity<String> connectTest(){
+        return ResponseEntity.status(HttpStatus.OK).body("연결에 성공하였습니다.");
+    }
+}
