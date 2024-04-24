d83979ac69b3265ce5f507a3ec86355eebcfd7e8
서성원 <zsa332@gmail.com>
2024-04-24 15:17:48 +0900

    Fix : Remove RequestMapping name #S10P31A605-90

--- backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/connect/ConnectController.java
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/connect/ConnectController.java
@@ -1,14 +1,13 @@
 package com.ssafy.igemoji.domain.connect;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "/connect")
+@RequestMapping("/connect")
 public class ConnectController {
     @GetMapping()
     public ResponseEntity<String> connectTest(){
