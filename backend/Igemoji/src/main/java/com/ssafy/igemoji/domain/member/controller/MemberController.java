a3c1b0df400eded1dcd0207475ac7ee325ca4db8
서성원 <zsa332@gmail.com>
2024-04-29 23:19:18 +0900

    FEAT : Add Update Nickname Method #S10P31A605-158

--- backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/member/controller/MemberController.java
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/member/controller/MemberController.java
@@ -1,6 +1,13 @@
 package com.ssafy.igemoji.domain.member.controller;
 
+import com.ssafy.igemoji.domain.member.dto.NicknameRequestDto;
+import com.ssafy.igemoji.domain.member.service.MemberService;
+import com.ssafy.igemoji.global.common.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/nickname")
+    public ResponseEntity<ResponseDto> updateNickname(@RequestBody NicknameRequestDto requestDto){
+        memberService.updateNickname(requestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("성공적으로 닉네임을 업데이트하였습니다."));
+    }
 }
+
--- backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/member/service/MemberService.java
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/member/service/MemberService.java
@@ -1,9 +1,28 @@
 package com.ssafy.igemoji.domain.member.service;
 
+import com.ssafy.igemoji.domain.member.Member;
+import com.ssafy.igemoji.domain.member.dto.NicknameRequestDto;
+import com.ssafy.igemoji.domain.member.repository.MemberRepository;
+import com.sun.jdi.InternalException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void updateNickname(NicknameRequestDto requestDto){
+        Member member = memberRepository.findById(requestDto.getMemberId()).orElseThrow(
+                () -> new InternalException("예외  처리 예정")
+        );
+
+        member.updateNickname(requestDto.getNickname());
+
+        memberRepository.save(member);
+    }
 }
+
