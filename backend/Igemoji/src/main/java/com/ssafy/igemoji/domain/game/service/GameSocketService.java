f2910c7523120fad438123de3b66a815145b7551
서성원 <zsa332@gmail.com>
2024-04-30 15:33:15 +0900

    FEAT : Add Service for Game Socket #S10P31A605-160

---
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/game/service/GameSocketService.java
@@ -0,0 +1,10 @@
+package com.ssafy.igemoji.domain.game.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GameSocketService {
+
+}
