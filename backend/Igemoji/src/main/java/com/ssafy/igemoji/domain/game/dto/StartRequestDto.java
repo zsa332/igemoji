d161ad45a64999858ee3f1119ff9a65984686f26
서성원 <zsa332@gmail.com>
2024-04-30 15:30:06 +0900

    FEAT : 게임 시작시 요청 DTO 추가 #S10P31A605-160

---
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/game/dto/StartRequestDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.igemoji.domain.game.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StartRequestDto {
+    private Integer roomId;
+    private Integer senderId;
+    private Integer questionNum;
+    private String genre;
+}
