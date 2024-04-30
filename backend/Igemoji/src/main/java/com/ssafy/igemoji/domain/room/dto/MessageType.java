38524ee6e9bf838161acba9d0aada72f1f1221a2
서성원 <zsa332@gmail.com>
2024-04-30 15:29:29 +0900

    FEAT : TIME_UPDATE ENUM 추가 #S10P31A605-160

--- backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/room/dto/MessageType.java
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/room/dto/MessageType.java
@@ -1,7 +1,8 @@
 package com.ssafy.igemoji.domain.room.dto;
 
 public enum MessageType {
-    ENTER_SUCCESS,
-    LEAVE_ROOM,
+    ENTER_SUCCESS, // 입장 성공
+    LEAVE_ROOM, // 방 퇴장
+    TIME_UPDATE, // 남은 시간 업데이트
 
 }
