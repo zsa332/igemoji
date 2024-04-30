996ce824ab6722dfd953a7f78f1ef4ce8aaf1ba9
서성원 <zsa332@gmail.com>
2024-04-30 15:32:16 +0900

    FEAT : 스케줄링 추가 #S10P31A605-160

--- backend/Igemoji/src/main/java/com/ssafy/igemoji/IgemojiApplication.java
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/IgemojiApplication.java
@@ -3,9 +3,11 @@ package com.ssafy.igemoji;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
 @EnableJpaAuditing
+@SpringBootApplication
 public class IgemojiApplication {
 
     public static void main(String[] args) {
