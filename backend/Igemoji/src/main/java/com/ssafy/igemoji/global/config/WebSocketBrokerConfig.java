ec346c2b4ad38ab5417a36be4f2a1e1531b9010a
서성원 <zsa332@gmail.com>
2024-05-02 13:17:33 +0900

    FEAT : withSockJs 추가

--- backend/Igemoji/src/main/java/com/ssafy/igemoji/global/config/WebSocketBrokerConfig.java
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/global/config/WebSocketBrokerConfig.java
@@ -22,7 +22,8 @@ public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
 }
