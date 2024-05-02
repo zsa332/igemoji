443594c9c4fe57b7067ebdf211830793d1418b24
서성원 <zsa332@gmail.com>
2024-05-03 01:09:19 +0900

    FEAT : 영화 문제 랜덤 추출 #S10P31A605-162

--- backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/movie/service/MovieService.java
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/movie/service/MovieService.java
@@ -1,7 +1,14 @@
 package com.ssafy.igemoji.domain.movie.service;
 
+import com.ssafy.igemoji.domain.movie.Emoji;
+import com.ssafy.igemoji.domain.movie.FamousLine;
 import com.ssafy.igemoji.domain.movie.dto.MovieResponseDto;
+import com.ssafy.igemoji.domain.movie.repository.EmojiRepository;
+import com.ssafy.igemoji.domain.movie.repository.FamousLineRepository;
+import com.ssafy.igemoji.domain.movie.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,9 +17,27 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MovieService {
 
-    public List<MovieResponseDto> makeQuetionList(){
+    private final MovieRepository movieRepository;
+    private final FamousLineRepository famousLineRepository;
+    private final EmojiRepository emojiRepository;
 
+    /* 영화 문제 랜덤 추출 */
+    public List<MovieResponseDto> getRandMovieList(int pageSize){
+        List<MovieResponseDto> movieList = movieRepository
+                .getRandMovieList(PageRequest.of(0, pageSize))
+                .stream()
+                .map(MovieResponseDto::toDto)
+                .toList();
 
-        return null;
+        for(MovieResponseDto movie : movieList){
+            FamousLine famousLine = famousLineRepository.getRandByMovieId(movie.getId())
+                    .orElse(null); // 아직 준비되지 않은 경우
+            movie.setRandLine(famousLine == null ? "준비 중 입니다" : famousLine.getLine());
+            Emoji emoji = emojiRepository.getRandByMovieId(movie.getId())
+                    .orElse(null); // 아직 준비되지 않은 경우
+            movie.setRandEmoji(emoji == null ? "준비 중 입니다" : emoji.getEmojiText());
+        }
+
+        return movieList;
     }
 }
