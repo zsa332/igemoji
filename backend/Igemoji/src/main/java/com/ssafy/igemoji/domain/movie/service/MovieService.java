6ad4c40dd055c461df4881637ef4761d48f97a51
서성원 <zsa332@gmail.com>
2024-05-02 17:58:12 +0900

    FEAT : 문제 생성 service #S10P31A605-162

--- backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/movie/service/MovieService.java
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/movie/service/MovieService.java
@@ -1,9 +1,18 @@
 package com.ssafy.igemoji.domain.movie.service;
 
+import com.ssafy.igemoji.domain.movie.dto.MovieResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
+
+    public List<MovieResponseDto> makeQuetionList(){
+
+
+        return null;
+    }
 }
