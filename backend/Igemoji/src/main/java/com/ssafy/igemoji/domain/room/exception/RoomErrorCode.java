d1048365719e911d0d44f93c2620344f344b5364
서성원 <zsa332@gmail.com>
2024-05-09 11:28:29 +0900

    FEAT : 비밀번호 error 추가 #S10P31A605-174

--- backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/room/exception/RoomErrorCode.java
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/room/exception/RoomErrorCode.java
@@ -4,7 +4,9 @@ import com.ssafy.igemoji.global.exception.ErrorCode;
 
 public enum RoomErrorCode implements ErrorCode {
     NOT_FOUND_ROOM("해당 방을 찾을 수 없습니다","ROM_001",404),
-    ROOM_FULL("해당 방의 인원이 초과하였습니다", "ROM_002", 403);
+    ROOM_FULL("해당 방의 인원이 초과하였습니다", "ROM_002", 403),
+    PASSWORD_INCORRECT("비밀번호가 틀려서 입장할 수 없습니다", "ROM_003", 403);
+
 
     private final String message;
     private final String errorCode;
