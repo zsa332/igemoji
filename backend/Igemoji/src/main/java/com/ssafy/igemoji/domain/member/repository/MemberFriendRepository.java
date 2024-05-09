0de59c8074a69e6147566ad4c3897655c5c7d03e
서성원 <zsa332@gmail.com>
2024-05-09 16:25:12 +0900

    Revert "Merge branch 'S10P31A605-174' into 'master'"
    This reverts merge request !66

--- backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/member/repository/MemberFriendRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.igemoji.domain.member.repository;
-
-import com.ssafy.igemoji.domain.member.MemberFriend;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberFriendRepository extends JpaRepository<MemberFriend, Integer> {
-}
